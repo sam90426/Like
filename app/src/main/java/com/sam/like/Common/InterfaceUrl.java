cda0fddb34e00432b05bdad7c4433d8e03d276f0
Sam <wxx90426@163.com>
2018-07-20 18:12:17 +0800

    wxx

--- app/src/main/java/com/sam/like/Common/InterfaceUrl.java
+++ app/src/main/java/com/sam/like/Common/InterfaceUrl.java
@@ -13,29 +13,28 @@ public class InterfaceUrl extends Application {
     //endregion
 
     //region 接口域名
-    //public static final String interfaceurl = "http://192.168.1.24:8086";
-    //public static final String interfaceurl="http://192.168.1.100:8081";
-    public static final String interfaceurl="http://123.207.50.130:8081";
+    public static final String interfaceurl = "http://192.168.1.110:8055";
+    //public static final String interfaceurl="http://123.207.50.130:8081";
     //endregion
 
     //region 接口地址
 
     //登录
-    public static final String logininterface = interfaceurl + "/account/login";
+    public static final String logininterface = interfaceurl + "/account/user/login";
     //注册
-    public static final String Registerinterface = interfaceurl + "/account/register";
+    public static final String Registerinterface = interfaceurl + "/account/user/register";
     //获取用户信息
-    public static final String GetUserInfointerface = interfaceurl + "/user/getuserinfo";
+    public static final String GetUserInfointerface = interfaceurl + "/user/getUserInfo";
     //首页列表
-    public static final String IndexCircleInterface = interfaceurl + "/index/index";
+    public static final String IndexCircleInterface = interfaceurl + "/index/findIndex";
     //关注好友
-    public static final String ApplyFriendInterface = interfaceurl + "/circle/friendrequests";
+    public static final String ApplyFriendInterface = interfaceurl + "/circle/friendApply";
     //动态点赞
-    public static final String CircleZanInterface = interfaceurl + "/circle/circlezan";
+    public static final String CircleZanInterface = interfaceurl + "/circle/circleZan";
     //取消点赞
-    public static final String CancelCircleZanInterface = interfaceurl + "/circle/cancelcirclezan";
+    public static final String CancelCircleZanInterface = interfaceurl + "/circle/cancelCircleZan";
     //好友动态
-    public static final String FriendCircleInterface = interfaceurl + "/circle/friendscircle";
+    public static final String FriendCircleInterface = interfaceurl + "/circle/friendsCircle";
     //官方文章列表
     public static final String ArticleListInterface = interfaceurl + "/article/articlelist";
     //官方文章点赞
@@ -43,19 +42,19 @@ public class InterfaceUrl extends Application {
     //官方文章取消点赞
     public static final String CancelArticleZanInterface = interfaceurl + "/article/delarticlezan";
     //好友列表
-    public static final String MyFriendsListInterface = interfaceurl + "/user/friendslist";
+    public static final String MyFriendsListInterface = interfaceurl + "/user/friendsList";
     //好友申请列表
-    public static final String FriendsApplyListInterface=interfaceurl+"/user/friendsapplylist";
+    public static final String FriendsApplyListInterface=interfaceurl+"/user/friendsApplyList";
     //好友操作
-    public static final String FriendsOperationInterface=interfaceurl+"/user/friendsoperation";
+    public static final String FriendsOperationInterface=interfaceurl+"/user/friendsOperation";
     //发布动态
     public static final String SendCircleInterface=interfaceurl+"/circle/sendcircle";
     //修改昵称
-    public static final String SetNickNameInterface=interfaceurl+"/user/updatenickname";
+    public static final String SetNickNameInterface=interfaceurl+"/user/updateNickName";
     //修改签名
-    public static final String SetSignInterface=interfaceurl+"/user/updatesign";
+    public static final String SetSignInterface=interfaceurl+"/user/updateSign";
     //修改密码
-    public static final String UpdatePwdInterface=interfaceurl+"/user/updatepwd";
+    public static final String UpdatePwdInterface=interfaceurl+"/user/updatePwd";
     //修改手机号码
     public static final String SetMobileInterface=interfaceurl+"/user/updatemobile";
     //修改电子邮箱
@@ -65,6 +64,6 @@ public class InterfaceUrl extends Application {
     //动态内容图片上传
     public static final String PublishTalkImageInterface=interfaceurl+"/circle/publishtalkimage";
     //发送动态评论
-    public static final String circlecommentInterface=interfaceurl+"/circle/circlecomment";
+    public static final String circlecommentInterface=interfaceurl+"/circle/circleComment";
     //endregion
 }
