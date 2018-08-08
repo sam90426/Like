b727ad972082ab91f509bba888d2049c237ba077
Sam <wxx90426@163.com>
2018-08-08 15:31:32 +0800

    updata

--- app/src/main/java/com/sam/like/View/Friend/VideoActivity.java
+++ app/src/main/java/com/sam/like/View/Friend/VideoActivity.java
@@ -3,6 +3,7 @@ package com.sam.like.View.Friend;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.MediaController;
 import android.widget.Toast;
@@ -10,6 +11,8 @@ import android.widget.VideoView;
 
 import com.sam.like.R;
 import com.sam.like.Utils.T;
+
+import java.io.File;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -23,8 +26,9 @@ public class VideoActivity extends AppCompatActivity {
         videoView = (VideoView) findViewById(R.id.play_video);
 
         String path = getIntent().getStringExtra("videoSavePath");
+        //String locaPath= Environment.getExternalStorageDirectory().getPath()+"/Download/F__Web_images_circleVideo_2018_8_6_15_1024253383858830.mp4";
 
-        Uri uri = Uri.parse(path);
+        //Uri uri = Uri.parse(path);
         //设置视频控制器
         videoView.setMediaController(new MediaController(this));
 
@@ -32,9 +36,11 @@ public class VideoActivity extends AppCompatActivity {
         videoView.setOnCompletionListener(new MyPlayerOnCompletionListener());
 
         //设置视频路径
-        videoView.setVideoURI(uri);
-        //videoView.setVideoPath(path);
-        videoView.requestFocus();
+        //videoView.setVideoURI(uri);
+        videoView.setVideoPath("http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear2/prog_index.m3u8");
+        //videoView.setVideoPath("http://192.168.1.110:8054/h5/img/1024253383858830.mp4");
+        //videoView.setVideoPath(file.getAbsolutePath());
+        //videoView.requestFocus();
 
         //开始播放视频
         videoView.start();
